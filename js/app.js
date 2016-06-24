f8316e168314a131d7e5bf773e186d7914435264
kaylynyuh <yuhkay4@gmail.com>
2016-06-24 10:40:56 -0700

    js functions refactored

--- js/app.js
+++ js/app.js
@@ -2,11 +2,9 @@
 
 var openHours = ['6am','7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
-// array for tracking shop hourly totals
 var shopHourlyTotals = new Array(15); // makes an empty array of length 15
 shopHourlyTotals.fill(0,0,15); // fills empty array with default values. value, starting index, 15
 
-// variable for tracking shop daily totals
 var shopDailyTotals = 0;
 
 function Shops(locationName, minCustPerHour, maxCustPerHour, avgCookiesPerCust) {
@@ -18,13 +16,13 @@ function Shops(locationName, minCustPerHour, maxCustPerHour, avgCookiesPerCust) 
   this.cookiesForEachHourArray = [];
   this.totalDailySales = 0;
 };
-//creating instances of Shops constructor
+
 var shopOne = new Shops('1st and Pike', 23, 65, 6.3);
 var shopTwo = new Shops('Seatac Airport', 3, 24, 1.2);
 var shopThree = new Shops('Seattle Center', 11, 38, 3.7);
 var shopFour = new Shops('Capitol Hill', 20, 38, 2.3);
 var shopFive = new Shops('Alki', 2, 16, 4.6);
-//using prototypal inheritance
+
 Shops.prototype.generateHourlyCustomer = function () {
   for(var i = 0; i < openHours.length; i++) {
     var custPerHour = Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour;
@@ -47,48 +45,28 @@ shopTwo.generateCookiesForEachHour();
 shopThree.generateCookiesForEachHour();
 shopFour.generateCookiesForEachHour();
 shopFive.generateCookiesForEachHour();
-//end constructor function
 
-//this accesses the table in the HTML
 var cookiesTable = document.getElementById('cookies');
-
-//this function handles the header content of the table
 function renderHeader () {
   var trEl = document.createElement('tr');
-
   var headerTitles = ['', 'Daily Location Total'];
   for(var i = 0; i < headerTitles.length; i++) {
-    //'for each pass, this code will run'
-    var thEl = document.createElement('th');//create
-    thEl.textContent = headerTitles[i];//add content
-    trEl.appendChild(thEl);//append this table header element into a row
+    buildElement('th', headerTitles[i], trEl);
   }
   for(var i = 0; i < openHours.length; i++) {
-    var thEl = document.createElement('th');
-    thEl.textContent = openHours[i];
-    trEl.appendChild(thEl);
+    buildElement('th', openHours[i], trEl);
   }
   cookiesTable.appendChild(trEl);
 };
 renderHeader();
-
 //this function handles the table data of the row
 function renderTableData (stores) {
   var trEl = document.createElement('tr');
-
-  var tdEl = document.createElement('td');//create
-  tdEl.textContent = stores.locationName;//add content
-  trEl.appendChild(tdEl);//append
-
-  var tdEl = document.createElement('td');
-  tdEl.textContent = stores.totalDailySales;
-  trEl.appendChild(tdEl);
+  buildElement('td', stores.locationName, trEl);
+  buildElement('td', stores.totalDailySales, trEl);
   shopDailyTotals += stores.totalDailySales;
-
   for(var i = 0; i < openHours.length; i++) {
-    var tdEl = document.createElement('td');
-    tdEl.textContent = stores.cookiesForEachHourArray[i];
-    trEl.appendChild(tdEl);
+    buildElement('td', stores.cookiesForEachHourArray[i], trEl);
     shopHourlyTotals[i] += stores.cookiesForEachHourArray[i];//why i needed array with default values
   }
   cookiesTable.appendChild(trEl);
@@ -96,19 +74,10 @@ function renderTableData (stores) {
 
 function renderTotals() {
   var trEl = document.createElement('tr');
-
-  var tdEl = document.createElement('td');//create
-  tdEl.textContent = 'Totals';//add content
-  trEl.appendChild(tdEl);//append
-
-  var tdEl = document.createElement('td');
-  tdEl.textContent = shopDailyTotals;
-  trEl.appendChild(tdEl);
-
+  buildElement('td', 'Totals', trEl);
+  buildElement('td', shopDailyTotals, trEl);
   for(var i = 0; i < shopHourlyTotals.length; i++) {
-    var tdEl = document.createElement('td');
-    tdEl.textContent = shopHourlyTotals[i];
-    trEl.appendChild(tdEl);
+    buildElement('td', shopHourlyTotals[i], trEl);
   }
   cookiesTable.appendChild(trEl);
 }
@@ -138,3 +107,10 @@ newStoreForm.addEventListener('submit', function(event) {
   newStore.generateCookiesForEachHour();
   renderTableData(newStore);
 });
+
+//global function to buil elements
+function buildElement (kind, content, where){
+  var x = document.createElement(kind);
+  x.textContent = content;
+  where.appendChild(x);
+}
